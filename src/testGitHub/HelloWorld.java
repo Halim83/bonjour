1d120ce3420570fa46b4cc37b3ef00550d76045d
ammour <ammour.halim@yahoo.fr>
2018-05-07 13:53:08 +0100

    step1

--- src/testGitHub/HelloWorld.java
+++ src/testGitHub/HelloWorld.java
@@ -3,7 +3,12 @@ package testGitHub;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-       System.out.println("hello world");
+		System.out.println("hello world");
+
+		try {
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 	}
 
 }
